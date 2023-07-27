9fd15257632ee3656ec808af23a60fa87913ede5
kanumuripa <kanumuripa@gmail.com>
2023-07-27 16:15:29 -0700

    first

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami                   = "ami-053b0d53c279acc90"
+  instance_type         = "t2.micro"
+  key_name              = "EC2-ssh"
+  vpc_security_group_ids = ["sg-04697372b9130adab"]
+
+
+  tags = {
+    Name = "app-server"
+  }
+}
